d09fc92d95530de71cd05999bd6e8a458d7ef7d0
mukulmh <mukulseu@gmail.com>
2022-12-20 19:09:23 +0600

    removed get mobile app link from header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -4,10 +4,6 @@
             33
             169 7720</a><a class="social-button sb-facebook shape-none sb-dark" href="#" target="_blank"><i class="socicon-facebook"></i></a><a class="social-button sb-twitter shape-none sb-dark" href="#" target="_blank"><i class="socicon-twitter"></i></a><a class="social-button sb-instagram shape-none sb-dark" href="#" target="_blank"><i class="socicon-instagram"></i></a><a class="social-button sb-pinterest shape-none sb-dark" href="#" target="_blank"><i class="socicon-pinterest"></i></a>
     </div>
-    <div class="topbar-column"><a class="hidden-md-down" href="#"><i class="icon-download"></i>&nbsp; Get mobile
-            app</a>
-
-    </div>
 </div>
 
 <header class="navbar mb-4">
